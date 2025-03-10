f4fb2312f3d87666c2e5df2c8107385184455d48
Kevin Daniel Arango Algarra <kevindanielarango@outlook.com>
2025-03-09 19:09:14 -0500

    Complemento Agregado

---
+++ script.js
@@ -0,0 +1,9 @@
+// Función para mostrar el mensaje de envío exitoso
+function mostrarMensaje(event) {
+    event.preventDefault();  // Evita que el formulario se envíe realmente (para propósitos de prueba)
+    
+    // Muestra el mensaje de "envío exitoso"
+    alert("Envío exitoso");
+
+    // Aquí puedes agregar más acciones si lo necesitas, como enviar el formulario a través de AJAX o redirigir.
+}
